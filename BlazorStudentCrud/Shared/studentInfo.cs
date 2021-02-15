b4cd19a0d35c660e9b5578bfb480ccbd232109f0
Debojit Paul <debojit162@gmail.com>
2021-02-15 13:27:49 +0530

    Blazor Web Assembly using Web API (CRUD operation)

---
+++ BlazorStudentCrud/Shared/studentInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+
+namespace BlazorStudentCrud.Shared
+{
+    public class studentInfo
+    {
+        public int stdId { get; set; }
+        public String studentName { get; set; }
+        public String course { get; set; }
+        public String year { get; set; }
+    }
+}
